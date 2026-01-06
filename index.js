b9b0ee9ac238c59cfffc5be2cb4ed3c5dd1bdac4
unknown <233922909+Balou1303@users.noreply.github.com>
2026-01-06 17:01:05 +0100

    feature/MVC-users

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) =>{
-    res.json({message : "Bienvenue dans l'API UnderSport"})
+app.get("/", (req, res) => {
+    res.json({ message: "Bienvenue dans l'API UnderSport" })
 });
 
-app.listen(process.env.SERVER_PORT, () =>{
+app.listen(process.env.SERVER_PORT, () => {
     console.log(`L'API est lancée sur http://localhost:${process.env.SERVER_PORT}`);
-    
+
 })
