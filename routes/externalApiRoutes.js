1f9fac8add5239ea9eac519e79f91edd4b037661
unknown <233922909+Balou1303@users.noreply.github.com>
2026-03-02 22:27:10 +0100

    features NBA

--- routes/externalApiRoutes.js
+++ routes/externalApiRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import externalApiController from '../controllers/externalApiController.js';
+import nbaController from '../controllers/nbaController.js';
 
 const router = express.Router();
 
 // Route pour récupérer le classement (ex: /api/external/standings ou /api/external/standings/FL1)
-// En Express 5, la syntaxe /:league? n'est plus supportée, il faut définir les deux routes explicitement
 router.get('/standings', externalApiController.getStandings);
 router.get('/standings/:league', externalApiController.getStandings);
 
@@ -15,5 +15,8 @@ router.get('/matches/:league', externalApiController.getMatches);
 // Route pour récupérer les matchs en direct (ex: /api/external/live ou /api/external/live/FL1)
 router.get('/live', externalApiController.getLiveMatches);
 router.get('/live/:league', externalApiController.getLiveMatches);
+
+// Route NBA
+router.get('/nba/matches/:date', nbaController.getMatches);
 
 export default router;
